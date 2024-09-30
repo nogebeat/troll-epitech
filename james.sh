9b574b1a5737e8474ac99b5fb0fbcf631b97b6b5
Noge <146479712+nogebeat@users.noreply.github.com>
2024-09-30 08:02:07 +0100

    Add files via upload

---
+++ james.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+trap '' INT
+
+stty intr ^C susp ^Z
+
+while true
+do
+    echo "PORNOOOOOOOOOOOOO "
+    sleep 1
+    echo " ON NE LAISSE JAMAIS SON PC OUVERT "
+    sleep 1
+    echo " 
+    
+    PETIT CADEAU DE NOGE "
+done
