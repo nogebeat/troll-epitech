d1f98e5e7b903120bc6f264f4a8c59f17781e7e0
Noge <146479712+nogebeat@users.noreply.github.com>
2025-10-10 07:17:45 +0100

    Create troll2.sh

---
+++ troll2.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Fichier cible (~/.bashrc ou ~/.zshrc)
+TARGET="$HOME/.bashrc"
+
+# Contenu du prank
+ANNIVERSAIRE_SCRIPT='
+# === Joyeux anniversaire (hidden fun) ===
+function anniversaire() {
+  for i in {1..50}; do
+    color=$((31 + RANDOM % 6))
+    echo -e "\033[${color}m🎉 Joyeux anniversaire 🎉 $USER \033[0m"
+    sleep 0.05
+  done
+}
+
+alias ls="anniversaire && command ls --color=auto"
+alias cd="anniversaire && builtin cd"
+alias git="anniversaire && command git"
+alias emacs="anniversaire && command emacs"
+'
+
+# Ajout de 60 lignes vides avant le contenu pour le "cacher"
+{
+  printf '\n%.0s' {1..260}
+  echo "$ANNIVERSAIRE_SCRIPT"
+} >> "$TARGET"
+
+echo "✅ Alias 'Joyeux anniversaire' ajouté discrètement dans $TARGET"
